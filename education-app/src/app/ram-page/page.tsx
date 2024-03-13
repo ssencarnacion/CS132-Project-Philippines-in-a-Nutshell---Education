81dc92a66fcc571b9157a4cb95060cdb7fe1105a
TheRedSwaRM <rsbalatibat@up.edu.ph>
2024-03-13 16:33:00 +0800

    chore: initial commit and setup of nextjs files and structure

---
+++ education-app/src/app/ram-page/page.tsx
@@ -0,0 +1,10 @@
+// app/home.tsx
+import Link from "next/link"
+
+export default function HomePage() {
+    return (<main>
+    <p>Hello, World! This is the //LEGIT NA TALAGA HAHA// Home Page!</p>
+    <br></br><br></br>
+    <Link href="/">Back</Link>
+    </main>);
+  }
