7e043860de0cc05646d67f5f32d666b796a34f4e
ssencarnacion <ssencarnacion1@up.edu.ph>
2024-04-14 10:19:41 +0800

    feat: steph playground initial layout

--- education-app/src/app/steph-page/page.tsx
+++ education-app/src/app/steph-page/page.tsx
@@ -1,11 +1,24 @@
-// app/home.tsx
-import Link from "next/link"
-
+import React from 'react';
+import './assets/css/styles.css'; 
 export default function HomePage() {
-    return (
-      <main>
-      <p>Hello, World! This is Steph's page playground (para ma-experiment natin yung framework here instead of sa main page hehe)</p>
-      <br></br><br></br>
-      <Link href="/">Back</Link>
-      </main>);
-  }
+  return (
+    
+          <body>
+              <title> CS 132: E-Youth </title>
+              <link rel="stylesheet" href="assets/css/styles.css" />
+              <link rel="preconnect" href="https://fonts.googleapis.com" />
+              <link rel="preconnect" href="https://fonts.gstatic.com"/>
+              <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+          
+             
+              <h1 className="title">E-YOUTH</h1>
+              <p className="subtext">EDUCATION OF THE YOUTH</p>
+              <p className="subtext2">Hello! We are a group of computer science students</p>
+              <div className="container">
+                  <a href="https://docs.google.com/document/d/1PHHBmDV-3PND5Hadw0EOlzy3_J7m5sKc3mBT6vxEaP0/edit" className="button">Data Set</a>
+                  <a href="https://docs.google.com/document/d/1PHHBmDV-3PND5Hadw0EOlzy3_J7m5sKc3mBT6vxEaP0/edit" className="button">Data Collection</a>
+              </div>
+          </body>
+      
+  );
+}
